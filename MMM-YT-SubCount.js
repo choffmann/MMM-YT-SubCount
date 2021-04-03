ee43cec6a7b0ded512f1b4dfb34778020cc686c9
Cedrik Hoffmann <cedrik.hoffmann@stud.hs-flensburg.de>
2021-04-03 17:03:27 +0200

    define npm test

--- MMM-YT-SubCount.js
+++ MMM-YT-SubCount.js
@@ -1,5 +1,3 @@
-/* global Module */
-
 /* Magic Mirror
  * Module: MMM-YT-SubCount
  *
@@ -8,106 +6,108 @@
  */
 
 Module.register("MMM-YT-SubCount", {
-	defaults: {
-		apiKey: '',
-		channelIds: [
-			{
-				id: ''
-			}
-		],
-		showChannelImg: true,
-		updateInterval: 60000,
-	},
+  defaults: {
+    apiKey: "",
+    channelIds: [
+      {
+        id: ""
+      }
+    ],
+    showChannelImg: true,
+    updateInterval: 60000
+  },
 
-	requiresVersion: "2.1.0", // Required version of MagicMirror
+  requiresVersion: "2.1.0", // Required version of MagicMirror
 
-	start: function () {
-		var self = this;
-		this.finalPayload = [];
+  start: function () {
+    var self = this;
+    this.finalPayload = [];
 
-		// Schedule update timer.
-		this.sendSocketNotification('MMM-YT-SubCount-HERE_IS_CONFIG', this.config);
-		setInterval(function () {
-			self.updateDom();
-		}, this.config.updateInterval);
-	},
+    // Schedule update timer.
+    this.sendSocketNotification("MMM-YT-SubCount-HERE_IS_CONFIG", this.config);
+    setInterval(function () {
+      self.updateDom();
+    }, this.config.updateInterval);
+  },
 
-	getDom: function () {
-		var self = this;
+  getDom: function () {
+    var self = this;
 
-		var wrapper = document.createElement("div");
-		wrapper.id = "MMM-YT-SubCount-root";
+    var wrapper = document.createElement("div");
+    wrapper.id = "MMM-YT-SubCount-root";
 
-		console.log(this.finalPayload)
-		if (this.finalPayload != undefined) {
-			this.finalPayload.items.forEach(item => {
-				var section = document.createElement("div");
-				section.id = "MMM-YT-SubCount-container";
+    console.log(this.finalPayload);
+    if (this.finalPayload !== undefined) {
+      this.finalPayload.items.forEach((item) => {
+        var section = document.createElement("div");
+        section.id = "MMM-YT-SubCount-container";
 
-				var img = document.createElement("div");
-				img.innerHTML = `<img src="${item.snippet.thumbnails.default.url}" width="${item.snippet.thumbnails.default.width}" height="${item.snippet.thumbnails.default.height}">`;
-				section.appendChild(img);
+        var img = document.createElement("div");
+        img.innerHTML = `<img src="${item.snippet.thumbnails.default.url}" width="${item.snippet.thumbnails.default.width}" height="${item.snippet.thumbnails.default.height}">`;
+        section.appendChild(img);
 
-				var content = document.createElement("div");
-				content.id = "MMM-YT-SubCount-content";
+        var content = document.createElement("div");
+        content.id = "MMM-YT-SubCount-content";
 
-				var title = document.createElement("p");
-				title.id = "MMM-YT-SubCount-title";
-				title.innerText = `${item.snippet.title}`
+        var title = document.createElement("p");
+        title.id = "MMM-YT-SubCount-title";
+        title.innerText = `${item.snippet.title}`;
 
-				var count = document.createElement("div");
-				count.id = "MMM-YT-SubCount-count";
-				count.innerHTML = `<p class="mdi mdi-youtube">${this.numFormatter(item.statistics.subscriberCount)}`
+        var count = document.createElement("div");
+        count.id = "MMM-YT-SubCount-count";
+        count.innerHTML = `<p class="mdi mdi-youtube">${this.numFormatter(
+          item.statistics.subscriberCount
+        )}`;
 
-				if (!this.config.showChannelImg) {
-					img.style.visibility = "hidden";
-				}
+        if (!this.config.showChannelImg) {
+          img.style.visibility = "hidden";
+        }
 
-				content.appendChild(title);
-				content.appendChild(count);
-				section.appendChild(content);
-				wrapper.appendChild(section)
-			})
-		}
+        content.appendChild(title);
+        content.appendChild(count);
+        section.appendChild(content);
+        wrapper.appendChild(section);
+      });
+    }
 
-		return wrapper;
-	},
+    return wrapper;
+  },
 
-	numFormatter: function (num) {
-		if (Math.abs(num) > 999999) {
-			return Math.sign(num) * ((Math.abs(num) / 1000000).toFixed(1)) + 'M'
-		} else if (Math.abs(num) > 999) {
-			return Math.sign(num) * ((Math.abs(num) / 1000).toFixed(1)) + 'K'
-		} else {
-			return Math.sign(num) * Math.abs(num)
-		}
-	},
+  numFormatter: function (num) {
+    if (Math.abs(num) > 999999) {
+      return Math.sign(num) * (Math.abs(num) / 1000000).toFixed(1) + "M";
+    } else if (Math.abs(num) > 999) {
+      return Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + "K";
+    } else {
+      return Math.sign(num) * Math.abs(num);
+    }
+  },
 
-	getScripts: function () {
-		return [];
-	},
+  getScripts: function () {
+    return [];
+  },
 
-	getStyles: function () {
-		return [
-			"MMM-YT-SubCount.css",
-			"https://cdn.jsdelivr.net/npm/@mdi/font@5.9.55/css/materialdesignicons.min.css",
-		];
-	},
+  getStyles: function () {
+    return [
+      "MMM-YT-SubCount.css",
+      "https://cdn.jsdelivr.net/npm/@mdi/font@5.9.55/css/materialdesignicons.min.css"
+    ];
+  },
 
-	// Load translations files
-	getTranslations: function () {
-		//FIXME: This can be load a one file javascript definition
-		return {
-			en: "translations/en.json",
-			es: "translations/es.json"
-		};
-	},
+  // Load translations files
+  getTranslations: function () {
+    //FIXME: This can be load a one file javascript definition
+    return {
+      en: "translations/en.json",
+      es: "translations/es.json"
+    };
+  },
 
-	// socketNotificationReceived from helper
-	socketNotificationReceived: function (notification, payload) {
-		if (notification === "MMM-YT-SubCount-DATA_IS_READY") {
-			this.finalPayload = payload;
-			this.updateDom();
-		}
-	},
+  // socketNotificationReceived from helper
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === "MMM-YT-SubCount-DATA_IS_READY") {
+      this.finalPayload = payload;
+      this.updateDom();
+    }
+  }
 });
--- node_helper.js
+++ node_helper.js
@@ -6,50 +6,50 @@
  */
 
 var NodeHelper = require("node_helper");
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 module.exports = NodeHelper.create({
+  start: function () {
+    this.finalData = [];
+    this.channelIds = [];
+    this.apiKey = null;
+  },
 
-	start: function () {
-		this.finalData = [];
-		this.channelIds = [];
-		this.apiKey = null;
-	},
+  socketNotificationReceived: function (notification, payload) {
+    switch (notification) {
+      case "MMM-YT-SubCount-HERE_IS_CONFIG":
+        this.apiKey = payload.apiKey;
+        this.channelIds = payload.channelIds;
+        this.breakDownChannelIds();
+        break;
+      case "UPDATE_PLEASE":
+        this.crypto();
+        break;
+    }
+  },
 
-	socketNotificationReceived: function (notification, payload) {
-		switch (notification) {
-			case "MMM-YT-SubCount-HERE_IS_CONFIG":
-				this.apiKey = payload.apiKey;
-				this.channelIds = payload.channelIds;
-				this.breakDownChannelIds();
-				break;
-			case "UPDATE_PLEASE":
-				this.crypto();
-				break;
-		}
-	},
+  breakDownChannelIds: function () {
+    this.url =
+      "https://youtube.googleapis.com/youtube/v3/channels?part=statistics&part=snippet&id=";
+    this.channelIds.forEach((channel) => {
+      this.url = this.url + "&id=" + channel.id;
+    });
+    this.getData(this.url);
+  },
 
-	breakDownChannelIds: function () {
-		this.url = "https://youtube.googleapis.com/youtube/v3/channels?part=statistics&part=snippet&id="
-		this.channelIds.forEach(channel => {
-			this.url = this.url + "&id=" + channel.id;
-		});
-		this.getData(this.url);
-	},
+  getData: function (url) {
+    fetch(url + "&key=" + this.apiKey, {
+      method: "GET",
+      headers: {
+        Accept: "application/json"
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => this.handleData(data))
+      .catch((error) => console.log("Error: ", error));
+  },
 
-	getData: function (url) {
-		fetch(url + "&key=" + this.apiKey, {
-			method: 'GET',
-			headers: {
-				"Accept": "application/json"
-			},
-		})
-			.then(response => response.json())
-			.then(data => this.handleData(data))
-			.catch(error => console.log('Error: ', error));
-	},
-
-	handleData: function (data) {
-		this.sendSocketNotification("MMM-YT-SubCount-DATA_IS_READY", data);
-	}
+  handleData: function (data) {
+    this.sendSocketNotification("MMM-YT-SubCount-DATA_IS_READY", data);
+  }
 });
